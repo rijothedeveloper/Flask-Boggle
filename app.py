ca5ef0a71eb9ac956a08eeb54c5cd0658b272f79
Rijo George <rijothedeveloper@gmail.com>
2021-12-09 17:51:23 -0800

    Displaying the Board

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from boggle import Boggle
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, session
 from flask_debugtoolbar import DebugToolbarExtension
+from boggle import Boggle
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "oh-so-secret"
@@ -10,4 +10,6 @@ boggle_game = Boggle()
 
 @app.route("/")
 def show_home():
-    return render_template("home.html")
+    board = boggle_game.make_board()
+    session["board"] = board
+    return render_template("home.html", board=board)
