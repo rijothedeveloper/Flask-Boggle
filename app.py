56b0b273ed9c06782943a8cd9d12c170a5661e54
Rijo George <rijothedeveloper@gmail.com>
2021-12-09 16:17:44 -0800

    Planning and Reading Code

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
 from boggle import Boggle
+from flask import Flask, request, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "oh-so-secret"
+debug = DebugToolbarExtension(app)
 
 boggle_game = Boggle()
