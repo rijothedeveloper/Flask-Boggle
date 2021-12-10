fd55d0d02ec4960f2c55f1ca8d95d712b408987b
Rijo George <rijothedeveloper@gmail.com>
2021-12-10 12:21:16 -0800

    test case done for all route

--- test.py
+++ test.py
@@ -6,5 +6,24 @@ from boggle import Boggle
 
 class FlaskTests(TestCase):
 
-    # TODO -- write tests for every view function / feature!
+    def test_homeScreen(self):
+        with app.test_client() as client:
+            resp = client.get("/")
+            html = resp.get_data(as_text=True)
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn('<p class="message">word already submitted</p>', html)
 
+    def test_valid_word(self):
+        with app.test_client() as client:
+            with client.session_transaction() as change_session:
+                change_session["board"] = Boggle().make_board()
+            resp = client.get("/valid_word",query_string=dict(word='hello'))
+            json = resp.get_data(as_text=True)
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn("result", json)
+
+    def gameEnd(self):
+        with app.test_client() as client:
+            resp = client.get("/game_end",query_string=dict(score='200'))
+            self.assertEqual(resp.status_code, 302)
+            self.assertEqual(session["highScore"], 200)
